bc0affe908e547a3c989f38e9199a639f2bf8b63
marcheesed <savajkiahalvorsen@outlook.com>
2026-01-05 02:57:48 +0000

    OOPS

--- app.py
+++ app.py
@@ -221,12 +221,12 @@ def admin_required(f):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("404.html"), 404
+    return render_template("errors/404.html"), 404
 
 
 @app.errorhandler(403)
 def access_denied(e):
-    return render_template("403.html"), 403
+    return render_template("errors/403.html"), 403
 
 
 @app.route("/accept_terms", methods=["GET", "POST"])
